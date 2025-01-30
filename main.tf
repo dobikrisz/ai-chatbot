a7ecf01b39868ac5b92ec0327adf1a450e781228
Krisztián  Dobos <krisztiandobos@Krisztians-MacBook-Air.local>
2025-01-31 00:08:20 +0100

    fixing typo

--- main.tf
+++ main.tf
@@ -1,14 +1,18 @@
+resource "random_string" "random" {
+  length           = 16
+  special          = true
+}
+
 # Cloud Run Service
-resource "google_cloud_run_service" "chatbot" {
+resource "google_cloud_run_v2_service" "chatbot" {
   name     = "chatbot-automated"
   location = var.LOCATION
 
   template {
-    spec {
-      containers {
-        image = "${var.LOCATION}-docker.pkg.dev/${var.PROJECT_ID}/cloud-run-chatbot-deploy/frontend:latest"
-      }
+    containers {
+      image = "${var.LOCATION}-docker.pkg.dev/${var.PROJECT_ID}/cloud-run-chatbot-deploy/frontend:latest"
     }
+    revision = random_string.random.result
   }
 
   traffic {
